94134830f21dd0a8b3c2a8be044861501dd28a75
Tammy Metz <tammy@tammymetz.com>
2018-09-22 22:22:44 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,8 +50,7 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
-    new_student = self.new(row[1], row[2])
-    new_student.id = row[0]
+    new_student = self.new(row[0], row[1], row[2])
     new_student
   end
 
